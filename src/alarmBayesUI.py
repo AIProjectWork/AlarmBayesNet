d312e89e0660136b9ba0154381c9c6689888ae70
HBB20 <harshbbhakta@gmail.com>
2017-11-14 18:54:47 -0600

    sampling rejection works

--- src/alarmBayesUI.py
+++ src/alarmBayesUI.py
@@ -1,9 +1,10 @@
-from alarmBayes import AlarmBayes
 import sys
-from myIO import MyIO
-from nodeUtil import NodeUtil
-from enumerationUtil import EnumerationUtil
 from __builtin__ import str
+
+from alarmBayes import AlarmBayes
+from enumerationUtil import EnumerationUtil
+from myIO import MyIO
+
 
 class AlarmBayesUI:
 #|-----------------------------------------------------------------------------|
@@ -27,6 +28,8 @@ class AlarmBayesUI:
         alarmBayes = AlarmBayes()
         # work on each query param turn by turn
         for query in query_params:
+            # result for query
+            print ("\n\n\n######## Result for query: {} ###########".format(alarmBayes.find_node(query).name))
             # enum result
             enumerationUtil = EnumerationUtil()
             
@@ -38,13 +41,19 @@ class AlarmBayesUI:
             sample_output, sample_rejection_output = \
                     enumerationUtil.result_for_sampling(query, evidences_input,\
                                                         alarmBayes, sample_list)
+            # sampling rejection
+            result_with_sample_rejection = \
+                enumerationUtil.result_for_sampling_rejection(query, evidences_input, \
+                                                              alarmBayes, sample_list)
             
             nSamples = len(sample_list)
             print ("\n------------------- sampling ---------------------------")
             myIO.print_sample_output(sample_output, nSamples)
             
             print("\n------------------ sample - rejection -------------------")
-            myIO.print_sample_output(sample_rejection_output, nSamples)
+            # myIO.print_sample_output(sample_rejection_output, nSamples)
+            myIO.print_sample_rejection_output(result_with_sample_rejection)
+
         #for query -ends
 #|------------------------alarmBayesUI -ends-----------------------------------|    
 
